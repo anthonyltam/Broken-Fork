5614abc394499531115c995761fc7ea7d1c3242a
anthony <anthonyltam@gmail.com>
2018-10-09 18:05:49 -0700

    incomplete user auth backend

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,25 @@
+class ApplicationController < ActionController::Base
+
+  def login(user)
+    @current_user = user
+    session[:session_token] = user.reset_session_token!
+  end
+
+  def current_user
+    @current_user ||= User.find_by_session_token(session[:session_token])
+  end
+
+  def require_login
+    redirect_to new_user_url unless current_user
+  end
+
+  def logged_in?
+    !!@current_user
+  end
+
+  def logout
+    current_user.reset_session_token!
+    session[:session_token] = nil
+  end
+
+end
