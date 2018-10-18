f7e7d7e057dee5ae76fa68fc226daff86d15e04b
anthony <anthonyltam@gmail.com>
2018-10-17 22:09:10 -0700

    some minor cs tweaks to rest splash

--- frontend/components/body/restaurant_splash_item.jsx
+++ frontend/components/body/restaurant_splash_item.jsx
@@ -7,9 +7,12 @@ class RestaurantSplashItem extends React.Component {
       <>
         <div className='rest-splash'>
           <img className='rest-splash-img' src={this.props.rest.photo_url}></img>
-          <h3>{this.props.rest.name}</h3>
-          {this.props.rest.cuisines}
-          {this.props.rest.location}
+          <div className='rest-splash-info'>
+            <h3 className='rest-splash-name'>{this.props.rest.name}</h3>
+            <div className='rest-splash-description'>{this.props.rest.cuisines} *$* {this.props.rest.location}</div>
+
+            <div className='book-today'>Book Today</div>
+          </div>
         </div>
       </>
     )
