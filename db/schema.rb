ea77b2e5b6d5f6612584ab6049d5bea5a5cb67e3
anthony <anthonyltam@gmail.com>
2018-11-11 22:46:34 -0800

    Add reviews table to db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_15_203847) do
+ActiveRecord::Schema.define(version: 2018_11_12_063837) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,13 @@ ActiveRecord::Schema.define(version: 2018_10_15_203847) do
     t.integer "tables", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.string "body", null: false
+    t.integer "rating", null: false
+    t.integer "restaurant_id", null: false
+    t.integer "author_id"
   end
 
   create_table "users", force: :cascade do |t|
