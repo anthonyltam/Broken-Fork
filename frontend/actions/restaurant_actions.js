1e8d1d8a2e7a086f12e1bf003ce18b2f7ef471a2
anthony <anthonyltam@gmail.com>
2018-11-12 10:21:54 -0800

    Review actions and apiutil nested under restaurants

--- frontend/actions/restaurant_actions.js
+++ frontend/actions/restaurant_actions.js
@@ -2,24 +2,34 @@ import * as APIUtil from '../util/restaurant_api_util';
 
 export const RECEIVE_RESTAURANTS = 'RECEIVE_RESTAURANTS';
 export const RECEIVE_RESTAURANT = 'RECEIVE_RESTAURANT';
+export const RECEIVE_REVIEW = 'RECEIVE_REVIEW';
 
 export const fetchRestaurants = () => dispatch => {
    return APIUtil.fetchRestaurants().then(restaurants => {
     dispatch({
       type: RECEIVE_RESTAURANTS,
       restaurants
-    })
-  })
-}
+    });
+  });
+};
 
 export const fetchRestaurant = (id) => dispatch => {
   APIUtil.fetchRestaurant(id).then(restaurant => {
     dispatch({
       type: RECEIVE_RESTAURANT,
       restaurant
-    })
-  })
-}
+    });
+  });
+};
+
+export const createReview = ( {review, author}) => dispatch => {
+  APIUtil.createReview(review).then(reviews => {
+    dispatch({
+      type: RECEIVE_REVIEW,
+      reviews
+    });
+  });
+};
 
 // export const receiveRestaurant = restaurant => ({
 //   type: RECEIVE_RESTAURANT,
@@ -31,3 +41,14 @@ export const fetchRestaurant = (id) => dispatch => {
 //     dispatch(receiveRestaurant(rest))
 //   ))
 // );
+
+
+// export const receiveReview = ({ review, average_rating, author }) => ({
+//   type: RECEIVE_REVIEW,
+//   review,
+//   average_rating,
+//   author
+// });
+
+// export const createReview = review => dispatch =>
+//   APIUtil.createReview(review).then(review => dispatch(receiveReview(review)));
--- frontend/util/restaurant_api_util.js
+++ frontend/util/restaurant_api_util.js
@@ -2,12 +2,20 @@ export const fetchRestaurants = () => {
   return $.ajax({
     method: "GET",
     url: `api/restaurants`
-  })
-}
+  });
+};
 
 export const fetchRestaurant = (id) => {
   return $.ajax({
     method: "GET",
     url: `api/restaurants/${id}`
-  })
-}
+  });
+};
+
+export const createReview = (review) => {
+  return $.ajax({
+    method: "POST",
+    url: `api/reviews`,
+    data: { review }
+  });
+};
