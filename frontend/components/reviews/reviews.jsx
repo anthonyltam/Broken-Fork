d19f57d07f82fd0c690ca1700c5282ab34a6e28c
anthony <anthonyltam@gmail.com>
2018-11-13 16:59:01 -0800

    Progress on review component styling

--- frontend/components/reviews/reviews.jsx
+++ frontend/components/reviews/reviews.jsx
@@ -3,22 +3,36 @@ import React from 'react';
 class Reviews extends React.Component {
 
   render() {
-    console.log('in reviews comp');
     return (
-      
       <>
-        <div className='review-box'>
+        <div className="review-box">
 
-          <div className='dined-on'>Dined on October 21st, 2018</div>
-          <div>Overall 5 |</div>
-          <div>Food 5 |</div>
-          <div>Service 3 |</div>
-          <div>Ambience 5 |</div>
-          <div className='review-body'>{this.props.review.body}</div>
-          <div className='review-rating'>{this.props.review.rating}</div>     
+          <div className='review-top-layer'>
+            <div className='review-stars'></div> 
+            <div className="dined-on">Dined on October 21st, 2018</div>
+          </div>
+          
+          <div className='review-categ'>
+            Overall<span className="review-ratings"> 5 </span>|
+            Food<span className="review-ratings"> 5 </span>|
+            Service<span className="review-ratings"> 3 </span>|
+            Ambience<span className="review-ratings"> 5 </span>
+          </div>
+ 
+          <div className="review-body">{this.props.review.body}</div>
+          {/* <div className="review-rating">{this.props.review.rating}</div> */}
+
+          <div className='review-btm-right'>
+            <div className='report-img'></div>
+            <div className='report-text'>Report</div>
+            <div className='helpful-img'></div>
+            <div className='report-text'>Helpful</div>
+          </div>
+          
         </div>
-        
-        <hr className='review-hr'/>
+
+        <hr className="review-hr" />
+
       </>
     );
   }
