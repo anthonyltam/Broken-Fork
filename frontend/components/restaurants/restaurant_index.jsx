3e5e47d7a3e02fee88061736a96ff2a14370cf70
anthony <anthonyltam@gmail.com>
2018-11-18 12:05:18 -0800

    final changes before seed

--- frontend/components/restaurants/restaurant_index.jsx
+++ frontend/components/restaurants/restaurant_index.jsx
@@ -29,25 +29,15 @@ class RestaurantIndex extends React.Component {
 
               <select className="rest-box-time">
                 <option value="12:00">12:00</option>
-                <option value="12:30">12:30</option>
                 <option value="1:00">1:00</option>
-                <option value="1:30">1:30</option>
                 <option value="2:00">2:00</option>
-                <option value="2:30">2:30</option>
                 <option value="3:00">3:00</option>
-                <option value="3:30">3:30</option>
                 <option value="4:00">4:00</option>
-                <option value="4:30">4:30</option>
                 <option value="5:00">5:00</option>
-                <option value="5:30">5:30</option>
                 <option value="6:00">6:00</option>
-                <option value="6:30">6:30</option>
                 <option value="7:00">7:00</option>
-                <option value="7:30">7:30</option>
                 <option value="8:00">8:00</option>
-                <option value="8:30">8:30</option>
                 <option value="9:00">9:00</option>
-                <option value="9:30">9:30</option>
                 <option value="10:00">10:00</option>
               </select>
 
@@ -62,7 +52,7 @@ class RestaurantIndex extends React.Component {
                 <option value="8">8 people</option>
               </select>
             </div>
-            
+
             <input className="rest-box-search" type="text" placeholder="Location, Restaurant, or Cuisine" />
             <button className="rest-box-go">Let's Go</button>
 
