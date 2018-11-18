89255fb842ca4f65d143977a509a74deb18b9e96
anthony <anthonyltam@gmail.com>
2018-11-17 18:23:37 -0800

    mild progress on reservation form

--- frontend/components/reservations/reservation_form.jsx
+++ frontend/components/reservations/reservation_form.jsx
@@ -20,25 +20,29 @@ class ReservationForm extends React.Component {
 
   handleClick(e) {
     e.preventDefault();
+    console.log('blue')
     this.props.createReservation(this.state);
 
     this.props.history.push("/reservations");
     
   }
 
-  update(field) {
-    return e => this.setState( { [field]: e.currentTarget.value });
+  update(event) {
+    let value = event.target.value;
+    this.setState( { start_time: value });
   }
 
+  // update(field) {
+  //   return e => this.setState( { [field]: e.currentTarget.value });
+  // }
+
   handleDayClick(date, { selected }) {
-    // console.log(this)
     this.setState({ day: date });
   }
 
   render() {
     console.log(this.state);
     return <>
-        {/* <div>hello from the reservation form</div> */}
         <div className="reservation-form-container">
           <form className="reservation-form-content" onSubmit={this.handleClick}>
             <div className="reservation-form-title">Make A Reservation</div>
@@ -47,22 +51,45 @@ class ReservationForm extends React.Component {
               <DayPicker 
                 onDayClick={this.handleDayClick}
                 selectedDays={this.state.day}
-                // disabledDays={{ daysOfWeek: [0] }}
               />
             </div>
 
-            {/* <TimePicker /> */}
+            {/* <div className="reservation-time-cont"> */}
+            <label className="reservation-time-text">Reservation Time</label>
+            {/* <input type="time" value={this.state.start_time} onChange={this.update("start_time")} placeholder="12" /> */}
+            <select className="reservation-time-option" onChange={ e => this.update(e)}>
+              {/* <option value="0">0:00</option>
+              <option value="1">1:00</option>
+              <option value="2">2:00</option>
+              <option value="3">3:00</option>
+              <option value="4">4:00</option>
+              <option value="5">5:00</option>
+              <option value="6">6:00</option>
+              <option value="7">7:00</option>
+              <option value="8">8:00</option>
+            <option value="9">9:00</option> */}
+              <option value="" selected>Please Select A Time</option>
+              <option value="10">10:00</option>
+              <option value="11">11:00</option>
+              <option value="12">12:00</option>
+              <option value="13">13:00</option>
+              <option value="14">14:00</option>
+              <option value="15">15:00</option>
+              <option value="16">16:00</option>
+              <option value="17">17:00</option>
+              <option value="18">18:00</option>
+              <option value="19">19:00</option>
+              <option value="20">20:00</option>
+              <option value="21">21:00</option>
+              <option value="22">22:00</option>
+              <option value="23">23:00</option>
+              <option value="24">24:00</option>
+            </select>
+            {/* </div> */}
 
+            <input className="reservation-submit-button" type="submit" value="Find A Table"></input>
 
-            {/* <input type="date" value={this.state.day} onChange={this.update("day")} /> */}
-
-            <label>Start Time</label>
-            <input type="time" value={this.state.start_time} onChange={this.update("start_time")} placeholder="12" />
-
-            {/* <label>End Time</label>
-            <input type="text" value={this.state.end_time} onChange={this.update("end_time")} placeholder="2" /> */}
-
-            <input className="Reservation-submit-button" type="submit" />
+            <div className="booked-times">Booked 10 times today</div>
           </form>
         </div>
       </>;
--- frontend/components/reservations/reservation_show_item.jsx
+++ frontend/components/reservations/reservation_show_item.jsx
@@ -1,9 +1,6 @@
 import React from 'react';
 
 class ReservationShowItem extends React.Component {
-  constructor(props) {
-    super(props);
-  }
 
   render() {
     return <>
@@ -14,11 +11,14 @@ class ReservationShowItem extends React.Component {
               <div>Day: {this.props.reservation.day}</div>
               <div>Time: {this.props.reservation.start_time}:00</div>
             </div>
+          </div>
 
+          <div className='delete-button'>
             <button className="delete-reservation" onClick={() => this.props.deleteReservation(this.props.reservation.id)}>
               X
             </button>
           </div>
+
         </div>
       </>;
   }
