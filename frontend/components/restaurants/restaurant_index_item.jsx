4112567a1b6f83bdf1a200eb305a470586cfcffa
anthony <anthonyltam@gmail.com>
2018-11-18 11:29:26 -0800

    rest index page restaurants styled

--- frontend/components/restaurants/restaurant_index_item.jsx
+++ frontend/components/restaurants/restaurant_index_item.jsx
@@ -10,13 +10,37 @@ class RestaurantIndexItem extends React.Component {
 
           <div className="rest-container">
             <img className='rest-image' src={this.props.restaurant.photo_url} ></img>
-            <Link to={`/restaurants/${this.props.restaurant.id}`} className="rest-name">{this.props.restaurant.name}</Link>
-                <div>Location: {this.props.restaurant.location}</div>
-                <div>Cuisines: {this.props.restaurant.cuisines}</div>
-                <div>Hours of Operation: {this.props.restaurant.hours_of_operation}</div>
-                <div>Tables: {this.props.restaurant.tables}</div>
+
+            <div className='rest-content-info'>
+              <Link to={`/restaurants/${this.props.restaurant.id}`} className="rest-name">{this.props.restaurant.name}</Link>
+
+              <div className="rest-star-cont">
+                <div className='rest-yellow-stars'></div>
+                <div className='rest-yellow-stars-description'>Excellent </div>
+              </div>
+
+              <div className="rest-loc-cui-row">
+                <div>{this.props.restaurant.cuisines}</div>
+                <div>{this.props.restaurant.location}</div>
+              </div>
+
+              <div className="booked-times-cont">
+                <div className="trend-arrow"></div>
+                <div>Booked # times today.</div>
+              </div>
+
+              <div className='rest-reservation-cont-row'>
+                <div className='rest-reserve-time'>6:30 PM</div>
+                <div className='rest-reserve-time'>6:45 PM</div>
+                <div className='rest-reserve-time'>7:00 PM </div>
+                <div className='rest-reserve-time'>7:15 PM </div>
+                <div className='rest-reserve-time'>7:30 PM</div>
+              </div>
+
+            </div>
+            
           </div>
-          
+
         </div>
 
       </>
