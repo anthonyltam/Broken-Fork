75a0c7fa12b473e934413ce169ac96dc00647c65
anthony <anthonyltam@gmail.com>
2019-02-04 19:27:39 -0800

    tweak review on home page

--- frontend/components/body/restaurant_splash_item.jsx
+++ frontend/components/body/restaurant_splash_item.jsx
@@ -13,7 +13,7 @@ class RestaurantSplashItem extends React.Component {
               <div className='rest-splash-name'>{this.props.rest.name}</div>
               <div className='rest-star-review'>
                 <div className='rest-star-icon'></div>
-                <div className='num-reviews'>100+ reviews</div>
+                <div className='num-reviews'>See Reviews</div>
               </div>
               <div className='rest-splash-description'>{this.props.rest.cuisines} *$* {this.props.rest.location}</div>
               <div className='book-today'>Book Today</div>
